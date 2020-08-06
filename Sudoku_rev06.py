9623b7ad1c06717ee6479365db674b71e244f087
Karl Smith <karl@karlsclipboard.com>
2020-08-05 21:48:18 -0400

    Day044 Code - Successfully solving for a single number.

--- Sudoku_rev06.py
+++ Sudoku_rev06.py
@@ -1,5 +1,7 @@
 # Sudoku_rev06.py - Sudoku Problem solver
 #
+#  20200805 - Working on the new idea of solving based on the highist existing
+#               number on the board.
 #  20200803 - Dug into numpy to count numbers in array - still working on code
 #  20200802 - Rev6 - total rebuild.  There is a logic error in finding the
 #               number to solve for.  The code is getting messy so taking
@@ -94,17 +96,28 @@ def check_3x3(point_y,point_x):
 #    print("The box is = ",box_y_start," ",box_y_end," ",box_x_start," ",box_x_end)
     box_3x3 = sudoku_9x9_in[box_y_start:box_y_end,box_x_start:box_x_end]
 #    print(box_3x3)
-    box_3x3 > 0
-    line_3x3 = np.reshape(box_3x3,(1,9))
-    sort_line = np.sort(line_3x3)
+#    box_3x3 > 0
+#    line_3x3 = np.reshape(box_3x3,(1,9))
+#    sort_line = np.sort(line_3x3)
 #    print(sort_line)
-    zeros_removed = sort_line[sort_line > 0]
+#    zeros_removed = sort_line[sort_line > 0]
 #    print("3x3 Check - zeros removed = ",zeros_removed)
     #row_list = zeros_removed
     #print("Row list in function: ",row_list)
-    return zeros_removed
+    return box_3x3
 
-
+def number_count():
+    number_hold = 0
+    for i in range (1, 10):
+        number_check = np.count_nonzero(sudoku_9x9_in == i)
+#        print("count ",i," = ",np.count_nonzero(sudoku_9x9_in == i))
+#        print("Count ",i," = ",number_check)
+#        print("Number Hold = ",number_hold)
+        if number_check > number_hold and number_check < 9:
+            george = i
+            number_hold = number_check
+#            print("Solve for = ",george)
+    return george
 
 
 
@@ -116,174 +129,109 @@ sudoku_9x9_in=sudoku_9x9_in.astype('int32')
 
 point_x = 0
 point_y = 0
-solved_count = 0
-not_solved_count = 0
-solved_hold = 0
+#solved_count = 0
+#not_solved_count = 0
+zero_hold = 0
+
+
+
+
+solve_number = number_count()
+print("Solve for = ",solve_number)
+
 
 while True:
-    point = sudoku_9x9_in[point_y,point_x]
-    print("Y=",point_y," X",point_x," value = ",point)
-    if point == 0:
-        print("Zero Found")
-    else:
-        print("Not a zero")
-
-
-    point_x += 1
-    if point_x == 9:
-        point_x = 0
+    #  ---- this is the row loop looking for the solve row
+    get_row = sudoku_9x9_in[point_y]
+    #print("Row Y=",point_y," = ",get_row)
+    #print(solve_number in get_row)
+    if solve_number in get_row:
         point_y += 1
-#        print("Y=",point_y," X",point_x," this should end")
         if point_y == 9:
-            print("Loop end break hit. Solved = ",solved_count)
-            if solved_hold == solved_count:
-                break
-            if not_solved_count > 0:
-                point_y = 0
-                solved_hold = solved_count
-                not_solved = 0
-                continue
-            else:
-                break
+            print("End Right Here")
+            break
         continue
+    else:
+        print("Row Y = ",point_y," possible solve")
+        print("Y=",point_y," X",point_x," value = ",solve_number)
+        #----------temp patch
+        if point_x == 9:
+            break
+        #------------------
 
 
-
-
-
-
-
-
-"""
-    if  point == 0:
-#        print("zero Found - Location:  Y=",point_y," X=",point_x)
-        zeros_removed = check_row(point_y)
-#        print(zeros_removed)
-        if len(zeros_removed) == 8:
-            insert_answer(point_y,point_x)
-            solved_count += 1
-            point_y += 1
-            point_x = 0
-            if point_y == 9:
-                point_y = 0
-                not_solved = 0
-#                print("Y=",point_y," X",point_x," Reset the loop")
-            continue   #forces restart of loop
-        else:
-            row_values = zeros_removed
-#        print("Multiple zeros in row check column")
-        zeros_removed = check_column(point_x)
-#        print("Column check = ",zeros_removed)
-        if len(zeros_removed) == 8:
-            insert_answer(point_y,point_x)
-            solved_count += 1
-            #point_y += 1
-            point_x += 1
-            if point_x == 9:
-                point_x = 0
-                point_y += 1
-#                print("Y=",point_y," X",point_x," this should end")
-                if point_y == 9:
-                    print("Column break hit")
-                    break
-            continue   #forces restart of loop
-        else:
-            column_values = zeros_removed
-            existing_values = np.concatenate((column_values,row_values))
-            existing_dups_removed = np.unique(existing_values)
-    #        print("Column and Row : ",existing_dups_removed)
-            if len(existing_dups_removed) == 8:
-                insert_answer(point_y,point_x)
-                solved_count += 1
-                point_y += 1
-                point_x = 0
-                if point_y == 9:
-                    point_y = 0
-                    not_solved = 0
-    #                print("Y=",point_y," X",point_x," Reset the loop")
-                continue   #forces restart of loop
-            else:
-                row_values = zeros_removed
-
-#        print("Multiple zeros in row and column check 3x3")
-        zeros_removed = check_3x3(point_y,point_x)
-#        print("Successfully returned from 3x3 check")
-        if len(zeros_removed) == 8:
-            insert_answer(point_y,point_x)
-            solved_count += 1
-            #point_y += 1
-            point_x += 1
-            if point_x == 9:
-                point_x = 0
-                point_y += 1
-#                print("Y=",point_y," X",point_x," this should end")
-                if point_y == 9:
-                    print("3x3 break hit")
-                    break
-            continue   #forces restart of loop
-        else:
-            values_3x3 = zeros_removed
-            all_existing = np.concatenate((values_3x3,existing_dups_removed))
-            all_uniques = np.unique(all_existing)
-            print("Y=",point_y," X=",point_x,"  All existing = ",all_uniques)
-
-            if len(all_uniques) == 8:
-                zeros_removed = all_uniques
-                insert_answer(point_y,point_x)
-                solved_count += 1
-            #    point_y += 1
+        # --- This is the column loop looking for a possible solve column
+        point_x = 0    #this resets the columns when swithing from row checks
+        while True:
+            get_column = sudoku_9x9_in[:,point_x]
+            #print("Column X = ",point_x," - ",get_column)
+            if solve_number in get_column:
                 point_x += 1
                 if point_x == 9:
-                    point_y += 1
-                    point_x = 0
-                    not_solved = 0
-                    if point_y == 9:
-                        print("3x3 break hit")
+                    break
+                continue
+            else:
+                print("Column X = ",point_x," possible solve")
+
+                # --- Row and Column found --- check the 3x3 loop
+                get_box = check_3x3(point_y,point_x)
+                print(get_box)
+                if solve_number in get_box:
+                    print("Solve Number is in the box")
+                    point_x += 1
+                    if point_x == 9:
                         break
-            #                print("Y=",point_y," X",point_x," Reset the loop")
-                continue   #forces restart of loop
+                    continue
+                else:
+                    # --- Double check the row
+                    get_row = sudoku_9x9_in[point_y]
+                    print("Double Check Row Y = ",point_y," - ",get_row)
+                    if solve_number in get_row:
+                        point_x += 1
+                        if point_x == 9:
+                            break
+                        continue
+                    else:
+                        print("Y=",point_y," X",point_x," value = ",solve_number)
+                        sudoku_9x9_in[point_y,point_x] = solve_number
+
+                        break
+
+
+        #  this next line will need to move
+        #point_y += 1
+
+
+    if point_y == 9:
+        break
+
+    continue
 
 
 
 
 
-            not_solved += 1
 
 
 
+print("The Final Answer:")
+print(sudoku_9x9_in)
 
 
-
-
-
-
-#            point_y += 1
-#            point_x = 0
-#            continue
-
+"""
     point_x += 1
     if point_x == 9:
         point_x = 0
         point_y += 1
 #        print("Y=",point_y," X",point_x," this should end")
         if point_y == 9:
-            print("Loop end break hit. Solved = ",solved_count)
-            if solved_hold == solved_count:
+            zeros_remaining = np.count_nonzero(sudoku_9x9_in == 0)
+            print("Loop End Zeros Remaining = ",zeros_remaining)
+            if zero_hold == zeros_remaining:
                 break
-            if not_solved > 0:
-                point_y = 0
-                solved_hold = solved_count
-                not_solved = 0
-                continue
             else:
-                break
+                zero_hold = zeros_remaining
+                point_y = 0
+                continue
         continue
-#    print("Y=",point_y," X=",point_x," at While end")
 """
-for i in range (0, 10):
-    print("count ",i," = ",np.count_nonzero(sudoku_9x9_in == i))
-
-print("The Fin)al Answer:")
-print("Solved for : ",solved_count)
-print("Still not solved : ",not_solved_count)
-print(sudoku_9x9_in)
